a1af9080e5626461cad0e7bf4363b39bc7c66d01
kmooney225 <kmooney225@gmail.com>
2022-09-01 13:01:38 -0600

    Added styling and gave app data from utils.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles/app.scss'
 import Player from './components/Player';
 import Song from './components/Song';
-import data from './utils';
+import data from './components/utils';
 
 function App() {
 
-
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
--- src/components/utils.js
+++ src/components/utils.js
@@ -1,21 +1,22 @@
 import { v4 as uuidv4 } from "uuid";
+import cover1 from "../res/ProfileMusicPic.jpg";
 
 function groove() {
     return [
         {
             name: "Thinking Out Loud",
-            cover: "./res/ProfileMusicPic.jpg",
+            cover: cover1,
             artist: "Kevin Mooney",
-            audio: "./res/I-am-not-the-only-one.wav",
+            audio: "../res/I-am-not-the-only-one.wav",
             color: ["#205950", "#2ab3bf"],
             id: uuidv4(),
             active: true,
         },
         {
             name: "I am Not the Only One",
-            cover: "./res/ProfileMusicPic.jpg",
+            cover: cover1,
             artist: "Kevin Mooney",
-            audio: "./res/Thinking-out-loud.wav",
+            audio: "../res/Thinking-out-loud.wav",
             color: ["#205950", "#2ab3bf"],
             id: uuidv4(),
             active: false,
