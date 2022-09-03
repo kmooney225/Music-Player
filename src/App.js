7b865ceeb356d97efde98646e8646f73d8091066
kmooney225 <kmooney225@gmail.com>
2022-09-03 12:08:22 -0600

    Added light/dark mode button

--- src/App.js
+++ src/App.js
@@ -13,12 +13,15 @@ function App() {
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [libraryStatus,setLibraryStatus] = useState(false)
+  const [darkMode,setDarkMode] = useState(false)
+
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
     AnimationPercentage: 0,
   })
-  const [libraryStatus,setLibraryStatus] = useState(false)
+
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
@@ -36,36 +39,43 @@ function App() {
   }
 
   return (
-    <div className={`App ${libraryStatus ? 'library-active' : "" }`}>
-      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus}/>
-      <Song currentSong={currentSong} />
-      <Player 
-      audioRef={audioRef}
-      setIsPlaying={setIsPlaying} 
-      isPlaying={isPlaying} 
-      currentSong={currentSong}
-      setSongInfo={setSongInfo}
-      songInfo={songInfo}
-      songs={songs}
-      setCurrentSong={setCurrentSong}
-      setSongs={setSongs}
-     />
-     <Library 
-     audioRef={audioRef} 
-     songs={songs} 
-     setCurrentSong={setCurrentSong} 
-     isPlaying={isPlaying}
-     setSongs={setSongs}
-     libraryStatus={libraryStatus}
-      />
-      <audio 
-        onTimeUpdate={timeUpdateHandler} 
-        onLoadedMetadata={timeUpdateHandler}
-        ref={audioRef} 
-        src={currentSong.audio}
-        onEnded={onEndedHandler}
-        >
-      </audio>
+    <div className={`App ${darkMode ? 'dark-active' : "" }`}>  
+      <div className={`App ${libraryStatus ? 'library-active' : "" }`}>
+        <Nav 
+          libraryStatus={libraryStatus} 
+          setLibraryStatus={setLibraryStatus}
+          darkMode={darkMode}
+          setDarkMode={setDarkMode}
+        />
+        <Song currentSong={currentSong} />
+        <Player 
+          audioRef={audioRef}
+          setIsPlaying={setIsPlaying} 
+          isPlaying={isPlaying} 
+          currentSong={currentSong}
+          setSongInfo={setSongInfo}
+          songInfo={songInfo}
+          songs={songs}
+          setCurrentSong={setCurrentSong}
+          setSongs={setSongs}
+        />
+        <Library 
+          audioRef={audioRef} 
+          songs={songs} 
+          setCurrentSong={setCurrentSong} 
+          isPlaying={isPlaying}
+          setSongs={setSongs}
+          libraryStatus={libraryStatus}
+        />
+        <audio 
+          onTimeUpdate={timeUpdateHandler} 
+          onLoadedMetadata={timeUpdateHandler}
+          ref={audioRef} 
+          src={currentSong.audio}
+          onEnded={onEndedHandler}
+          >
+        </audio>
+      </div>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,25 @@
 import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import { faMusic } from "@fortawesome/free-solid-svg-icons"
+import { faMusic,faMoon,faSun } from "@fortawesome/free-solid-svg-icons"
 
-const Nav = ({setLibraryStatus, libraryStatus}) => {
+const Nav = ({setLibraryStatus, libraryStatus, setDarkMode, darkMode }) => {
+
+    const mode = () => {
+        if(darkMode){
+            return 'Light Mode'
+        }
+        else{
+            return 'Dark Mode'
+        }
+    }
+
     return(
         <nav>
             <h1>Kevin's Audio</h1>
+            <button onClick={() => setDarkMode(!darkMode)}>
+                {mode()}
+                <FontAwesomeIcon icon={darkMode ? faMoon : faSun} transform="right-4" />
+            </button>
             <button onClick={() => setLibraryStatus(!libraryStatus)}>
                 Library
                 <FontAwesomeIcon icon={faMusic} />
